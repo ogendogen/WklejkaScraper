8f4745db4a4788270500a9396679a5a5ccdfc558
ogendogen <kosmos500@interia.pl>
2020-11-19 00:00:30 +0100

    Process method

--- Core/Crawler.cs
+++ Core/Crawler.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using Core.Models;
 
 namespace Core
 {
     public class Crawler
     {
+        public Config Config { get; set; }
+        public Crawler(Config config)
+        {
+            Config = config;
+        }
+
+        public List<Entry> Process()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
