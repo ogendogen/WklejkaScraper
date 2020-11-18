5d78d6f29ce44530721a7b37f5a89b1255f9b35a
ogendogen <kosmos500@interia.pl>
2020-11-18 23:23:21 +0100

    Added "Entry" model

---
+++ Core/Models/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class Entry
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+}
