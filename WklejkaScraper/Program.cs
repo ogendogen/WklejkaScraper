50fb2872d3e6f16cb0a8397b3ffb4a3b1c68ed61
ogendogen <kosmos500@interia.pl>
2020-11-18 22:59:04 +0100

    Add project files.

---
+++ WklejkaScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WklejkaScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
