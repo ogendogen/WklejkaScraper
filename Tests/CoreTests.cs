66418d93d3bc9e01e04cc565e79a6eda09e9ce27
ogendogen <kosmos500@interia.pl>
2020-11-20 23:19:06 +0100

    Created tests project

---
+++ Tests/CoreTests.cs
@@ -0,0 +1,14 @@
+using Core;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class CoreTests
+    {
+        [Test]
+        public void SinglePageTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
