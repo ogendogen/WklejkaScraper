c7b670fa321c143471213266c6e641cf865cc469
ogendogen <kosmos500@interia.pl>
2020-11-20 15:35:17 +0100

    Config.MaxTriesPerPage

--- Core/Models/Config.cs
+++ Core/Models/Config.cs
@@ -12,5 +12,6 @@ namespace Core.Models
         public string ContentPath { get; set; }
         public string VariableSymbol { get; set; }
         public int PagesAmount { get; set; }
+        public int MaxTriesPerPage { get; set; }
     }
 }
