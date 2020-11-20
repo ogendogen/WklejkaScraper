0b612f05ecec7f3376ff31225b5d6d194dac2d58
ogendogen <kosmos500@interia.pl>
2020-11-20 14:56:33 +0100

    Added RequestPageContent

--- Core/Crawler.cs
+++ Core/Crawler.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using Core.Models;
 
 namespace Core
@@ -15,6 +18,21 @@ namespace Core
         public List<Entry> Process()
         {
             throw new NotImplementedException();
+        }
+
+        private async Task<RequestResult> RequestPageContent(string page)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(page);
+            request.Method = "GET";
+            using (var response = await request.GetResponseAsync())
+            using (var stream = response.GetResponseStream())
+            using (var reader = new StreamReader(stream))
+            {
+                HttpStatusCode statusCode = ((HttpWebResponse)response).StatusCode;
+                string contents = reader.ReadToEnd();
+
+                return new RequestResult() { StatusCode = (int)statusCode, Content = contents};
+            }
         }
     }
 }
