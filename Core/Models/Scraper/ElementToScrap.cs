2db4e0fb1fbbd3950c29bd15461abb8024360049
ogendogen <kosmos500@interia.pl>
2020-11-20 16:20:53 +0100

    Models for scraper

---
+++ Core/Models/Scraper/ElementToScrap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.Scraper
+{
+    public class ElementToScrap
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Regex { get; set; }
+    }
+}
---
+++ Core/Models/Scraper/ScrapedElement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.Scraper
+{
+    public class ScrapedElement
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
