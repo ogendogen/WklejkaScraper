c89576376a7ecd442e8e40280db403f1ec7a793e
ogendogen <kosmos500@interia.pl>
2020-11-21 18:36:56 +0100

    Added FailedEntry

---
+++ Core/Models/Crawler/Entries/FailedEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Crawler.Interfaces;
+
+namespace Core.Models.Crawler.Entries
+{
+    public class FailedEntry : IEntry
+    {
+        public int ID { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
