451c61656aa8bb2aeef6eea15bd28ad20676df88
ogendogen <kosmos500@interia.pl>
2020-11-21 19:54:30 +0100

    Added alternative text to find to element to scrap

--- Core/Models/Scraper/Elements/ElementToScrap.cs
+++ Core/Models/Scraper/Elements/ElementToScrap.cs
@@ -8,7 +8,8 @@ namespace Core.Models.Scraper.Elements
     {
         public string Name { get; set; }
         public string Path { get; set; }
-        public string AlternativePath { get; set; }
         public string Regex { get; set; }
+        public string AlternativePath { get; set; }
+        public string AlternativeTextToFind { get; set; }
     }
 }
