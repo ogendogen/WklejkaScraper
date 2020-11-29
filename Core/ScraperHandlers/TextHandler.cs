9c5bbda2b2a6461799cc9b841aa43c7a59ce97c4
ogendogen <kosmos500@interia.pl>
2020-11-30 00:11:38 +0100

    TextHandler

---
+++ Core/ScraperHandlers/TextHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Scraper.Abstract;
+using Core.Models.Scraper.Elements;
+using HtmlAgilityPack;
+
+namespace Core.ScraperHandlers
+{
+    internal class TextHandler : ScraperHandler
+    {
+        public override object Handle(object request)
+        {
+            HtmlDocument htmlDoc = (HtmlDocument)request;
+            HtmlNode htmlNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[2]/div[3]/table/tbody[1]/tr/td[2]/pre");
+            if (htmlNode != null)
+            {
+                return new ScrapedTextElement()
+                {
+                    Name = "Content",
+                    Content = htmlNode.InnerText
+                };
+            }
+            
+            return base.Handle(request);
+        }
+    }
+}
