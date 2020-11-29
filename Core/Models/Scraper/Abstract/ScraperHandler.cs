14dc48505dd5d18a9144990dfa1ca432a15327be
ogendogen <kosmos500@interia.pl>
2020-11-30 00:06:13 +0100

    Interface and abstract class for scraper's chain of responsiblity

---
+++ Core/Models/Scraper/Abstract/ScraperHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Scraper.Interfaces;
+
+namespace Core.Models.Scraper.Abstract
+{
+    internal abstract class ScraperHandler : IScraperHandler
+    {
+        private IScraperHandler _nextHandler;
+
+        public IScraperHandler SetNext(IScraperHandler handler)
+        {
+            _nextHandler = handler;
+            
+            return handler;
+        }
+        
+        public virtual object Handle(object request)
+        {
+            if (_nextHandler != null)
+            {
+                return _nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Core/Models/Scraper/Interfaces/IScraperHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.Scraper.Interfaces
+{
+    interface IScraperHandler
+    {
+        IScraperHandler SetNext(IScraperHandler handler);
+        object Handle(object request);
+    }
+}
