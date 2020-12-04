475b8772e1ad2bd9b32681610caabfa83a0ce016
ogendogen <kosmos500@interia.pl>
2020-12-04 18:09:09 +0100

    Created handlers

---
+++ Core/ScraperHandlers/DeletedHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Scraper.Abstract;
+using Core.Models.Scraper.Elements;
+using HtmlAgilityPack;
+
+namespace Core.ScraperHandlers
+{
+    internal class DeletedHandler : ScraperHandler
+    {
+        public override object Handle(object request)
+        {
+            HtmlDocument htmlDoc = (HtmlDocument)request;
+            HtmlNode htmlNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[2]/div[3]/table/tbody[1]/tr/tbody/tr/td/img");
+            if (htmlNode != null)
+            {
+                return new ScrapedTextElement()
+                {
+                    Name = "Content",
+                    Content = "deleted"
+                };
+            }
+
+            return base.Handle(request);
+        }
+    }
+}
---
+++ Core/ScraperHandlers/PictureHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Scraper.Abstract;
+using Core.Models.Scraper.Elements;
+using HtmlAgilityPack;
+
+namespace Core.ScraperHandlers
+{
+    internal class PictureHandler : ScraperHandler
+    {
+        public override object Handle(object request)
+        {
+            HtmlDocument htmlDoc = (HtmlDocument)request;
+            HtmlNode htmlNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[2]/div[3]/table/tbody[1]/tr/tbody/tr/td/img");
+            if (htmlNode != null)
+            {
+                return new ScrapedPictureElement()
+                {
+                    Name = "Content",
+                    Path = htmlNode.GetAttributeValue("src", "empty picture")
+                };
+            }
+            
+            return base.Handle(request);
+        }
+    }
+}
