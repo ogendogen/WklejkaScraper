fe87bf30e71c1ad674d83df63602b4d50c3b2865
ogendogen <kosmos500@interia.pl>
2020-12-04 18:37:30 +0100

    Created author and date handlers

---
+++ Core/ScraperHandlers/DateHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Scraper.Abstract;
+using Core.Models.Scraper.Elements;
+using Core.Models.Scraper.Interfaces;
+using HtmlAgilityPack;
+
+namespace Core.ScraperHandlers
+{
+    internal class DateHandler : ScraperHandler
+    {
+        public override IScrapedElement Handle(HtmlDocument htmlDoc)
+        {
+            HtmlNode htmlNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div[2]/div[3]/table/thead/tr/td/table/tbody/tr/td[1]");
+            if (htmlNode != null)
+            {
+                return new ScrapedTextElement()
+                {
+                    Name = "Content",
+                    Content = htmlNode.InnerText
+                };
+            }
+            
+            return base.Handle(htmlDoc);
+        }
+    }
+}
