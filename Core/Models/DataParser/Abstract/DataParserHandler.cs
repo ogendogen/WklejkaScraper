469a914ce5802b5af5ce8cadda40bd174f4ccc8d
ogendogen <kosmos500@interia.pl>
2020-12-05 19:15:54 +0100

    Data parser handlers

---
+++ Core/Models/DataParser/Abstract/DataParserHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.DataParser.Interfaces;
+using Core.Models.Scraper.Interfaces;
+
+namespace Core.Models.DataParser.Abstract
+{
+    internal abstract class DataParserHandler : IDataParserHandler
+    {
+        private IDataParserHandler _nextHandler;
+
+        public IDataParserHandler SetNext(IDataParserHandler handler)
+        {
+            _nextHandler = handler;
+            
+            return handler;
+        }
+        
+        public virtual IEntry Handle(IScrapedElement scrapedElement)
+        {
+            if (_nextHandler != null)
+            {
+                return _nextHandler.Handle(scrapedElement);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Core/Models/DataParser/Interfaces/IDataParserHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models.Scraper.Interfaces;
+
+namespace Core.Models.DataParser.Interfaces
+{
+    interface IDataParserHandler
+    {
+        IDataParserHandler SetNext(IDataParserHandler handler);
+        IEntry Handle(IScrapedElement scrapedElement);
+    }
+}
