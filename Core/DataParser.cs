87ef2d4df80f105efb14ec2ee8bc149cb7bf6de9
ogendogen <kosmos500@interia.pl>
2020-12-06 18:36:54 +0100

    DataParser refactor for chain of responsibility

--- Core/DataParser.cs
+++ Core/DataParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using Core.DataParserHandlers;
 using Core.Models;
 using Core.Models.DataParser.Entries;
 using Core.Models.DataParser.Interfaces;
@@ -21,57 +22,48 @@ namespace Core
         internal IEntry GetEntryByScrapedElements(List<IScrapedElement> scrapedElements, int id)
         {
             string content = String.Empty;
+            
+            IEntry entry = GetContent(scrapedElements, id);
+            string author = GetAuthor(scrapedElements);
+            DateTime date = GetDate(scrapedElements);
+
+            if (entry is PictureEntry pictureEntry)
+            {
+                pictureEntry.Author = author;
+                pictureEntry.Date = date;
+                return pictureEntry;
+            }
+
+            if (entry is TextEntry textEntry)
+            {
+                textEntry.Author = author;
+                textEntry.Date = date;
+                return textEntry;
+            }
+
+            return entry;
+        }
+
+        private IEntry GetContent(List<IScrapedElement> scrapedElements, int id)
+        {
             var contentElement = scrapedElements.FirstOrDefault(el => el.Name == "Content");
-            if (contentElement != null)
-            {
-                // determine type of content
-                if (contentElement is ScrapedPictureElement pictureElement)
-                {
-                    return new PictureEntry()
-                    {
-                        ID = id,
-                        PicturePath = pictureElement.Path
-                        // todo: OCR here
-                    };
-                }
-                else if (contentElement is ScrapedTextElement textElement)
-                {
-                    if (textElement.Content == "deleted")
-                    {
-                        return new DeletedEntry()
-                        {
-                            ID = id
-                        };
-                    }
-                    else if (textElement.Content == "error")
-                    {
-                        return new FailedEntry()
-                        {
-                            ID = id,
-                            StatusCode = -1
-                        };
-                    }
-                    else if (textElement.Content == "password")
-                    {
-                        return new PasswordProtectedEntry()
-                        {
-                            ID = id
-                        };
-                    }
-                    else
-                    {
-                        content = textElement.Content;
-                    }
-                }
-            }
+            
+            var pictureHandler = new PictureEntryHandler();
+            var deletedHandler = new DeletedEntryHandler();
+            var passwordHandler = new PasswordEntryHandler();
+            var errorHandler = new ErrorEntryHandler();
+            var textHandler = new TextEntryHandler();
 
-            string author = String.Empty;
-            var authorElement = scrapedElements.FirstOrDefault(el => el.Name == "Author");
-            if (authorElement != null && authorElement is ScrapedTextElement authorTextElement)
-            {
-                author = ParseAuthor(authorTextElement);
-            }
+            pictureHandler.SetNext(deletedHandler)
+                .SetNext(passwordHandler)
+                .SetNext(errorHandler)
+                .SetNext(textHandler);
 
+            return pictureHandler.Handle(contentElement, id);
+        }
+
+        private DateTime GetDate(List<IScrapedElement> scrapedElements)
+        {
             DateTime date = new DateTime();
             var dateElement = scrapedElements.FirstOrDefault(el => el.Name == "Date");
             if (dateElement != null && dateElement is ScrapedTextElement dateTextElement)
@@ -79,13 +71,19 @@ namespace Core
                 date = ParseDate(dateTextElement);
             }
 
-            return new TextEntry()
+            return date;
+        }
+
+        private string GetAuthor(List<IScrapedElement> scrapedElements)
+        {
+            string author = String.Empty;
+            var authorElement = scrapedElements.FirstOrDefault(el => el.Name == "Author");
+            if (authorElement != null && authorElement is ScrapedTextElement authorTextElement)
             {
-                ID = id,
-                Author = author,
-                Date = date,
-                Content = content
-            };
+                author = ParseAuthor(authorTextElement);
+            }
+
+            return author;
         }
 
         private DateTime ParseDate(ScrapedTextElement dateTextElement)
