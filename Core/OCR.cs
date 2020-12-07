3b741a2229a1d35187f73bf7f00d7300bfd035de
ogendogen <kosmos500@interia.pl>
2020-12-07 23:52:50 +0100

    OCR static class

---
+++ Core/OCR.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    internal static class OCR
+    {
+        private static readonly HttpClient client = new HttpClient();
+        internal static async Task<string> ProcessImage(string apiKey, byte[] image)
+        {
+            client.DefaultRequestHeaders.Add("apikey", apiKey);
+            var values = new Dictionary<string, string>
+            {
+                { "base64Image", Convert.ToBase64String(image) },
+                { "language", "pol" },
+                { "filetype", "gif" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.PostAsync("https://api.ocr.space/parse/image", content);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
