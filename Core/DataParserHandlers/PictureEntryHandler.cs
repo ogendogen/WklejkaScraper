fd97db321bd47e2ab55e2de9d0520fcf8d57f06a
ogendogen <kosmos500@interia.pl>
2020-12-07 22:09:11 +0100

    Scrap image

--- Core/DataParserHandlers/PictureEntryHandler.cs
+++ Core/DataParserHandlers/PictureEntryHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Core.Models.DataParser.Abstract;
 using Core.Models.DataParser.Entries;
 using Core.Models.DataParser.Interfaces;
@@ -11,19 +12,32 @@ namespace Core.DataParserHandlers
 {
     internal class PictureEntryHandler : DataParserHandler
     {
+        public RequestHandler RequestHandler { get; set; }
+        public PictureEntryHandler(int maxTries)
+        {
+            RequestHandler = new RequestHandler(maxTries);
+        }
+
         public override IEntry Handle(IScrapedElement scrapedElement, int id)
         {
             if (scrapedElement is ScrapedPictureElement pictureElement)
             {
+                byte[] pictureBytes = DownloadPicture(pictureElement.Path);
                 return new PictureEntry()
                 {
                     ID = id,
-                    PicturePath = pictureElement.Path
+                    PicturePath = pictureElement.Path,
+                    Picture = pictureBytes
                     // todo: OCR here
                 };
             }
 
             return base.Handle(scrapedElement, id);
+        }
+
+        private byte[] DownloadPicture(string path)
+        {
+            return RequestHandler.GetImage(path);
         }
     }
 }
