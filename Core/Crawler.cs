5a26e7159cdc940369272ab6fcb0d5928975593a
ogendogen <kosmos500@interia.pl>
2020-12-12 20:18:33 +0100

    Multithreading with thread pool

--- Core/Crawler.cs
+++ Core/Crawler.cs
@@ -9,6 +9,7 @@ using Core.Models.DataParser.Entries;
 using Core.Models.DataParser.Interfaces;
 using Core.Models.Scraper;
 using Core.Models.Scraper.Elements;
+using System.Threading;
 
 namespace Core
 {
@@ -18,30 +19,81 @@ namespace Core
         internal RequestHandler RequestHandler { get; private set; }
         internal Scraper Scraper { get; set; }
         internal DataParser DataParser { get; set; }
+        public List<IEntry> ProcessedEntries { get; set; }
+
+        private object _locker = new object();
+        //private ManualResetEvent _resetEvent = new ManualResetEvent(false);
+        private List<ManualResetEvent> _resetEvents;
         public Crawler(Config config)
         {
             Config = config;
             Configuration();
+            ProcessedEntries = new List<IEntry>();
+            _resetEvents = new List<ManualResetEvent>();
         }
 
-        public async IAsyncEnumerable<IEntry> Process()
+        public void Process()
         {
-            for (int i=Config.StartPageId; i<=Config.EndPageId; i++)
+            //ParallelLoopResult parallelResult = Parallel.For(Config.StartPageId, Config.EndPageId, new ParallelOptions() { MaxDegreeOfParallelism = 2 }, i =>  // await GetEntry(i));
+            //{
+            //    lock(_locker)
+            //    {
+            //        ProcessedEntries.Add(GetEntry(i));
+            //    }
+            //});
+
+            //await Task.WhenAll(ProcessedEntries);
+            //ThreadPool.SetMaxThreads(4, 4);
+            
+            //for (int i = Config.StartPageId; i <= Config.EndPageId; i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem(new WaitCallback(async state => await GetEntry(i)));
+            //}
+
+            Barrier barrier = new Barrier(Config.EndPageId - Config.StartPageId + 1);
+            for (int i = Config.StartPageId; i <= Config.EndPageId; i++)
+            {
+                //ThreadPool.QueueUserWorkItem(new WaitCallback(async state => await GetEntry(i)));
+                ThreadPool.QueueUserWorkItem(
+                    async arg =>
+                    {
+                        int pageNumber = (int)arg;
+                        Console.WriteLine("Queued ID " + pageNumber);
+                        var entry = await GetEntry(pageNumber);
+                        Console.WriteLine("Done ID " + pageNumber);
+                        ProcessedEntries.Add(entry);
+                        barrier.SignalAndWait();
+                    }, i);
+            }
+            barrier.SignalAndWait();
+        }
+
+        private async Task<IEntry> GetEntry(int i)
+        {
+            try
             {
                 var requestResult = await RequestHandler.GetPageContent(i);
                 if (requestResult.StatusCode == 200)
                 {
                     var scrapedElements = Scraper.ScrapAllFromContent(requestResult.Content).ToList();
-                    yield return DataParser.GetEntryByScrapedElements(scrapedElements, i);
+                    return DataParser.GetEntryByScrapedElements(scrapedElements, i);
                 }
                 else
                 {
-                    yield return new FailedEntry()
+                    return new FailedEntry()
                     {
                         ID = i,
                         StatusCode = requestResult.StatusCode
                     };
                 }
+            }
+            catch (Exception ex)
+            {
+                return new FailedEntry()
+                {
+                    ID = i,
+                    StackTrace = ex.StackTrace
+                };
             }
         }
 
--- WklejkaScraper/Program.cs
+++ WklejkaScraper/Program.cs
@@ -6,6 +6,7 @@ using Core;
 using Core.Models;
 using Core.Models.DataParser.Interfaces;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace WklejkaScraper
 {
@@ -23,31 +24,32 @@ namespace WklejkaScraper
 
             string configJson = File.ReadAllText("config.json");
             Config config = JsonConvert.DeserializeObject<Config>(configJson);
-            
+
             Crawler crawler = new Crawler(config);
-            List<IEntry> entries = new List<IEntry>();
 
             decimal counter = 0;
             decimal maxPages = config.EndPageId - config.StartPageId;
             decimal percentage = 0;
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            
-            await foreach (var entry in crawler.Process())
-            {
-                Console.SetCursorPosition(0, 0);
-                Console.WriteLine($"Processed pages: {counter} / {maxPages}");
-                Console.WriteLine($"Processed pages: {percentage.ToString("F")}%");
 
-                entries.Add(entry);
-                
-                counter++;
-                percentage = (counter / maxPages) * 100;
-            }
+            crawler.Process();
+            List<IEntry> entries = crawler.ProcessedEntries;
+            //await foreach (var entry in crawler.Process())
+            //{
+            //    Console.SetCursorPosition(0, 0);
+            //    Console.WriteLine($"Processed pages: {counter} / {maxPages}");
+            //    Console.WriteLine($"Processed pages: {percentage.ToString("F")}%");
+
+            //    entries.Add(entry);
+
+            //    counter++;
+            //    percentage = (counter / maxPages) * 100;
+            //}
 
             watch.Stop();
             Console.WriteLine("Finished");
-            Console.WriteLine($"Seconds taken: {watch.ElapsedMilliseconds / 1000}");
+            Console.WriteLine($"Time taken: {watch.Elapsed.Hours}h {watch.Elapsed.Minutes}m {watch.Elapsed.Seconds}s");
             Console.ReadKey();
         }
     }
