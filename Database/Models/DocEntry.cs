5b7f3f5838d149b30e75ef029e03b80cc7bab7df
ogendogen <kosmos500@interia.pl>
2020-12-19 18:29:08 +0100

    Created db models

---
+++ Database/Models/DocEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Database.Models
+{
+    public class DocEntry
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public int ID { get; set; }
+        public int PageID { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public string PicturePath { get; set; }
+        public byte[] Picture { get; set; }
+    }
+}
---
+++ Database/Models/FailedDoc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Database.Models
+{
+    public class FailedDoc
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public int ID { get; set; }
+        public int PageID { get; set; }
+        public int StatusCode { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
