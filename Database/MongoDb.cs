e6952847d52347af187d484433d065d29c2aa044
ogendogen <kosmos500@interia.pl>
2020-12-19 18:33:33 +0100

    MongoDb constructor

--- Database/MongoDb.cs
+++ Database/MongoDb.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using Database.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Core;
 
 namespace Database
 {
     public class MongoDb
     {
+        public MongoClient MongoClient { get; set; }
+        public IMongoDatabase MongoDatabase { get; set; }
+        public IMongoCollection<DocEntry> DocsCollection { get; set; }
+        public IMongoCollection<FailedDoc> FailedDocsCollection { get; set; }
+        
+        public MongoDb(string connectionString)
+        {
+            MongoClient = new MongoClient(connectionString);
+            
+            MongoDatabase = MongoClient.GetDatabase("wklejto");
+            DocsCollection = MongoDatabase.GetCollection<DocEntry>("docs");
+            FailedDocsCollection = MongoDatabase.GetCollection<FailedDoc>("failed_docs");
+        }
     }
 }
